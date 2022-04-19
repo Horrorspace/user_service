ccc90da34c1ae3d328bdb8d7fb31ed9987deef98
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-04-19 21:57:26 +0700

    create user scheme

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,37 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Schema as MongoShema } from 'mongoose';
+import { RefreshToken } from './refresh-token.schema';
+import { Settings } from './settings.schema';
+
+export type UserDocument = User & Document;
+
+const { ObjectId } = MongoShema.Types;
+
+@Schema()
+export class User {
+    @Prop()
+    login: string;
+
+    @Prop()
+    email: string;
+
+    @Prop()
+    password: string;
+
+    @Prop()
+    phone: string;
+
+    @Prop()
+    confirmationCode: string;
+
+    @Prop()
+    confirmed: boolean;
+
+    @Prop({ type: [ObjectId], ref: RefreshToken.name })
+    refreshTokens: RefreshToken[];
+
+    @Prop({ type: ObjectId, ref: Settings.name })
+    settings: Settings;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
