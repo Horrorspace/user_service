f7c247fa896e6849438dfd72555c0e9fae2179df
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-04-19 21:57:51 +0700

    create refresh-token scheme

---
+++ src/schemas/refresh-token.schema.ts
@@ -0,0 +1,33 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Schema as MongoShema } from 'mongoose';
+import { User } from './user.schema';
+
+export type RefreshTokenDocument = RefreshToken & Document;
+
+const { ObjectId } = MongoShema.Types;
+
+@Schema()
+export class RefreshToken {
+    @Prop({ type: ObjectId, ref: 'User' })
+    userId: User;
+
+    @Prop()
+    token: string;
+
+    @Prop()
+    fingerprint: string;
+
+    @Prop()
+    userAgeng: string;
+
+    @Prop()
+    ip: string;
+
+    @Prop(Date)
+    createdAt: Date;
+
+    @Prop()
+    expiresIn: number;
+}
+
+export const RefreshTokenSchema = SchemaFactory.createForClass(RefreshToken);
