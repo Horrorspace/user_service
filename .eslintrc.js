713277bfec11f7e6c2bf97ba9ae9c3728e459bf3
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-04-21 22:04:36 +0700

    extends has been disabled

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,9 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'prettier'
+    // 'plugin:@typescript-eslint/recommended',
+    // 'plugin:prettier/recommended',
+    // 'prettier'
   ],
   root: true,
   env: {
@@ -22,5 +22,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    "no-console": "off"
   },
 };
