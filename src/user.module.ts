f78944da9aab6d0b3be1c9f914fc7573dc5d2449
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-04-21 22:05:01 +0700

    add schemas

--- src/user.module.ts
+++ src/user.module.ts
@@ -1,13 +1,36 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, ModelDefinition } from '@nestjs/mongoose';
+import { UserSchema, User } from './schemas/user.schema';
+import { SettingsSchema, Settings } from './schemas/settings.schema';
+import {
+    RefreshTokenSchema,
+    RefreshToken,
+} from './schemas/refresh-token.schema';
+
+const mongoUri: string = process.env.MONGODB_URI!;
+const mongoOptions: ModelDefinition[] = [
+    {
+        name: User.name,
+        schema: UserSchema,
+    },
+    {
+        name: RefreshToken.name,
+        schema: RefreshTokenSchema,
+    },
+    {
+        name: Settings.name,
+        schema: SettingsSchema,
+    },
+];
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             envFilePath: '.env',
         }),
-        MongooseModule.forRoot(process.env.MONGODB_URI!),
+        MongooseModule.forRoot(mongoUri),
+        MongooseModule.forFeature(mongoOptions),
     ],
     controllers: [],
     providers: [],
