226a201443f0d0dda47a7d4f07274ba19ecc0191
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2022-04-22 09:44:12 +0700

    Update create-user.dto.ts

--- src/dto/create-user.dto.ts
+++ src/dto/create-user.dto.ts
@@ -2,5 +2,4 @@ export class CreateUserDto {
     public readonly login: string;
     public readonly email: string;
     public readonly password: string;
-    public readonly phone: string | null;
 }
