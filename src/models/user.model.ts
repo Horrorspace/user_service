342a0914e0f96c77c3744a6cb1f92c3681c36197
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-04-23 12:16:04 +0700

    added create method

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,12 +2,18 @@ import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { UserDocument, User } from '../schemas/user.schema';
+import { CreateUserDto } from 'src/dto/create-user.dto';
 
 @Injectable()
 export class UserModel {
     constructor(
         @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
     ) {}
+
+    async create(userDto: CreateUserDto): Promise<UserDocument> {
+        const createdUser = new this.userModel(userDto);
+        return createdUser.save();
+    }
 
     async readAll(): Promise<User[]> {
         return this.userModel.find().exec();
@@ -20,4 +26,6 @@ export class UserModel {
     async readByEmail(email: string): Promise<User | null> {
         return this.userModel.findOne({ email }).exec();
     }
+
+    async deleteByLogin(login: string): Promise<void> {}
 }
