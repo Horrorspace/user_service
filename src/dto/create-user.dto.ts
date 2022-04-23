08f68dc81d2d27d3d931f0dcaedefea83448bee8
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-04-23 14:30:42 +0700

    edit phone field

--- src/dto/create-user.dto.ts
+++ src/dto/create-user.dto.ts
@@ -2,5 +2,5 @@ export class CreateUserDto {
     public readonly login: string;
     public readonly email: string;
     public readonly password: string;
-    public readonly phone: string | null;
+    public readonly phone?: string;
 }
