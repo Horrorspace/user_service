996d0507eafff1d9db9799b9a678e3a7279693ec
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-04-24 12:38:25 +0700

    edit

--- src/refresh-token/models/refresh-token.model.ts
+++ src/refresh-token/models/refresh-token.model.ts
@@ -1,40 +1,71 @@
-import { Model, ObjectId } from 'mongoose';
+import { Model, ObjectId, ProjectionType, FilterQuery } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { RefreshTokenDocument, RefreshToken, refreshTokenName } from '../schemas/refresh-token.schema';
+import {
+    RefreshTokenDocument,
+    RefreshToken,
+    refreshTokenName,
+} from '../schemas/refresh-token.schema';
+import { CreateRefreshTokenDto } from '../dto/create-refresh-token.dto';
 
 @Injectable()
 export class RefreshTokenModel {
     constructor(
-        @InjectModel(refreshTokenName) private readonly refreshTokenModel: Model<RefreshTokenDocument>,
+        @InjectModel(refreshTokenName)
+        private readonly refreshTokenModel: Model<RefreshTokenDocument>,
     ) {}
 
-    async create(userDto: CreateUserDto): Promise<UserDocument> {
-        return await this.userModel.create(userDto);
+    async create(
+        refreshTokenDto: CreateRefreshTokenDto,
+    ): Promise<RefreshToken> {
+        return await this.refreshTokenModel.create(refreshTokenDto);
     }
 
     async readAll(): Promise<RefreshToken[]> {
         return await this.refreshTokenModel.find().exec();
     }
 
-    async readByUserId(userId: ObjectId): Promise<RefreshToken | null> {
-        return await this.refreshTokenModel.findOne({ userId }).exec();
+    async readByUserId(userId: ObjectId): Promise<RefreshToken[]> {
+        return await this.refreshTokenModel.find({ userId }).exec();
     }
 
     async readById(_id: ObjectId): Promise<RefreshToken | null> {
         return await this.refreshTokenModel.findOne({ _id }).exec();
     }
 
-    async updateByUserId(userId: ObjectId, data: UpdateUserDto): Promise<boolean> {
-        const result = await this.userModel
-            .updateOne({ login }, { $set: data })
+    async deleteByUserId(userId: ObjectId): Promise<boolean> {
+        const result = await this.refreshTokenModel
+            .deleteMany({ userId })
             .exec();
-        if (result.modifiedCount === 1) return true;
+        if (result.deletedCount > 0) return true;
         else return false;
     }
 
-    async deleteByUserId(userId: ObjectId): Promise<boolean> {
-        const result = await this.refreshTokenModel.deleteMany({ userId }).exec();
+    async deleteById(_id: ObjectId): Promise<boolean> {
+        const result = await this.refreshTokenModel.deleteOne({ _id }).exec();
+        if (result.deletedCount === 1) return true;
+        else return false;
+    }
+
+    async deleteExceptLastByUserId(userId: ObjectId): Promise<boolean> {
+        const filter: FilterQuery<RefreshTokenDocument> = { userId };
+        const projection: ProjectionType<RefreshTokenDocument> = {
+            createdAt: 1,
+        };
+        const [{ createdAt: lastes }] = await this.refreshTokenModel
+            .find(filter, projection)
+            .sort({ createdAt: 'descending' })
+            .exec();
+        const lastesDate = lastes.toISOString();
+        const deleteFilter: FilterQuery<RefreshTokenDocument> = {
+            userId,
+            createdAt: {
+                $lt: lastesDate,
+            },
+        };
+        const result = await this.refreshTokenModel
+            .deleteMany(deleteFilter)
+            .exec();
         if (result.deletedCount > 0) return true;
         else return false;
     }
