69040bcfcd371df74ac8327a7e3c924436cf0c23
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-04-24 12:40:29 +0700

    refresh-token model has been removed

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,23 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule, ModelDefinition } from '@nestjs/mongoose';
 import { UserSchema, userName } from './schemas/user.schema';
-import {
-    SettingsSchema,
-    settingsName,
-} from './schemas/settings.schema';
-import {
-    RefreshTokenSchema,
-    refreshTokenName,
-} from '../refresh-token/schemas/refresh-token.schema';
+import { SettingsSchema, settingsName } from './schemas/settings.schema';
 
 const mongoOptions: ModelDefinition[] = [
     {
         name: userName,
         schema: UserSchema,
-    },
-    {
-        name: refreshTokenName,
-        schema: RefreshTokenSchema,
     },
     {
         name: settingsName,
