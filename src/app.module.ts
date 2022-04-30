9ab7697796f04f8bc97b3a1ecee93eef9e151ddb
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-04-30 18:10:50 +0700

    Add RefreshTokenModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { RefreshTokenModule } from './refresh-token/refresh-token.module';
 
 const mongoUri: string = process.env.MONGODB_URI!;
 
@@ -12,6 +13,7 @@ const mongoUri: string = process.env.MONGODB_URI!;
         }),
         MongooseModule.forRoot(mongoUri),
         UserModule,
+        RefreshTokenModule,
     ],
     controllers: [],
     providers: [],
