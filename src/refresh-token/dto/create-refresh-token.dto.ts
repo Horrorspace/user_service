f3e74255745722e485be06ced1b3cc95ed6d90d7
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-04-30 18:10:16 +0700

    edit

--- src/refresh-token/dto/create-refresh-token.dto.ts
+++ src/refresh-token/dto/create-refresh-token.dto.ts
@@ -4,7 +4,7 @@ export class CreateRefreshTokenDto {
     public readonly userId: objectId;
     public readonly token: string;
     public readonly fingerprint?: string;
-    public readonly userAgeng: string;
+    public readonly userAgent: string;
     public readonly ip: string;
     public readonly expiresIn: number;
 }
