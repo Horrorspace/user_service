cae1821568ab3d213f63df598f6471a107adef5e
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-06-13 16:44:20 +0700

    fix bugs

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,8 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestMicroservice } from '@nestjs/common';
-import { ClientsModule, Transport, ClientProxy } from '@nestjs/microservices'; 
+import { ClientsModule, Transport, ClientProxy } from '@nestjs/microservices';
 import { AppModule } from '../src/app.module';
-import { options } from '../src/main';
+import { getOptions } from '../src/main';
 import { Observable } from 'rxjs';
 import { IUserRes } from 'src/user/interfaces/IUserRes';
 
@@ -11,24 +11,27 @@ describe('AppController (e2e)', () => {
     let client: ClientProxy;
     const userService = 'user_service';
 
-   beforeAll(async () => {
+    beforeAll(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
             imports: [
                 AppModule,
-                ClientsModule.register([{
-                    name: userService,
-                    transport: Transport.RMQ,
-                    options: {
-                        urls: ['amqp://localhost:5672'],
-                        queue: 'user',
-                        queueOptions: {
-                            durable: true,
+                ClientsModule.register([
+                    {
+                        name: userService,
+                        transport: Transport.RMQ,
+                        options: {
+                            urls: ['amqp://localhost:5672'],
+                            queue: 'user',
+                            queueOptions: {
+                                durable: true,
+                            },
                         },
                     },
-                }])
+                ]),
             ],
         }).compile();
 
+        const options = await getOptions();
         app = moduleFixture.createNestMicroservice(options);
         await app.init();
 
@@ -38,14 +41,17 @@ describe('AppController (e2e)', () => {
     afterAll(async () => {
         await app.close();
         await client.close();
-    })
+    });
 
-    it('should return not found code', done => {
-        const response: Observable<IUserRes> = client.send('getUserByEmail', 'tester@test.com');
-        response.subscribe(data => {
+    it('should return not found code', (done) => {
+        const response: Observable<IUserRes> = client.send(
+            'getUserByEmail',
+            'tester@test.com',
+        );
+        response.subscribe((data) => {
             expect(data).toBeDefined();
             expect(data).toEqual('404');
             done();
-        })
+        });
     });
 });
