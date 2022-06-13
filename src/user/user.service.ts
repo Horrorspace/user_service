c7b1bf5fda2c97a39187e3c0290a3f6e2cc2438d
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-06-13 22:02:49 +0700

    add createUserByEmail cmd

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,17 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { QueryBus } from '@nestjs/cqrs';
+import { QueryBus, CommandBus } from '@nestjs/cqrs';
 import { GetUserByEmailQuery } from './queries/impl/get-user-by-email.query';
 import { GetUserByLoginQuery } from './queries/impl/get-user-by-login.query';
+import { CreateUserByEmailCommand } from './commands/impl/create-user-by-email.command';
 import { IUserRes } from './interfaces/IUserRes';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
-    constructor(@Inject(QueryBus) private readonly queryBus: QueryBus) {}
+    constructor(
+        @Inject(QueryBus) private readonly queryBus: QueryBus,
+        @Inject(CommandBus) private readonly commandBus: CommandBus,
+    ) {}
 
     async getUserByEmail(email: string): Promise<IUserRes> {
         return await this.queryBus.execute<GetUserByEmailQuery, IUserRes>(
@@ -17,6 +22,12 @@ export class UserService {
     async getUserByLogin(login: string): Promise<IUserRes> {
         return await this.queryBus.execute<GetUserByLoginQuery, IUserRes>(
             new GetUserByLoginQuery(login),
+        );
+    }
+
+    async createUserByEmail(userToCreate: CreateUserDto): Promise<IUserRes> {
+        return await this.commandBus.execute<CreateUserByEmailCommand, IUserRes>(
+            new CreateUserByEmailCommand(userToCreate),
         );
     }
 }
