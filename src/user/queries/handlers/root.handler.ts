89d2c5e4b3922e89aab52075ec943a8a57984622
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-06-13 22:02:21 +0700

    add GetUserByLoginHandler

--- src/user/queries/handlers/root.handler.ts
+++ src/user/queries/handlers/root.handler.ts
@@ -1,3 +1,4 @@
 import { GetUserByEmailHandler } from './get-user-by-email.handler';
+import { GetUserByLoginHandler } from './get-user-by-login.handler';
 
-export const QueryHandlers = [GetUserByEmailHandler];
+export const QueryHandlers = [GetUserByEmailHandler, GetUserByLoginHandler];
