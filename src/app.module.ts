7bf4325021fd2fabc4e8f71042d3d651e26aeaeb
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-06-13 16:43:49 +0700

    edit config and mongoose modules options

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,39 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { RefreshTokenModule } from './refresh-token/refresh-token.module';
-
-const mongoUri: string =
-    process.env.MONGODB_URI || 'mongodb://user:BGNdWquZ@localhost:2717';
+import configuration from './config/configuration';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
-            envFilePath: '.env',
+            isGlobal: true,
+            ignoreEnvFile: true,
+            load: [configuration],
         }),
-        MongooseModule.forRoot(mongoUri),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => {
+                const host =
+                    process.env.MONGODB_HOST ||
+                    configService.get('mongodb.host');
+                const port =
+                    process.env.MONGODB_PORT ||
+                    configService.get('mongodb.port');
+                const user =
+                    process.env.MONGODB_USER ||
+                    configService.get('mongodb.user');
+                const password =
+                    process.env.MONGODB_PASSWORD ||
+                    configService.get('mongodb.password');
+                const uri = `mongodb://${user}:${password}@${host}:${port}`;
+                return {
+                    uri,
+                };
+            },
+        }),
         UserModule,
         RefreshTokenModule,
     ],
