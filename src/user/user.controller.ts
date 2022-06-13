7eb59865366a5b7894e7a4201d43bc6bef03f550
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-06-13 22:03:00 +0700

    add createUserByEmail cmd

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,6 +9,8 @@ import { UserExceptionFilter } from './filters/user-exception.filter';
 import { JsonInterceptor } from './interceptors/json.interceptor';
 import { IUserRes } from './interfaces/IUserRes';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserPipe } from './pipes/create-user.pipe'
 
 @Controller()
 export class UserController {
@@ -28,5 +30,12 @@ export class UserController {
     @UseInterceptors(JsonInterceptor)
     async getUserByLogin(@Payload() login: string): Promise<IUserRes> {
         return this.userService.getUserByEmail(login);
+    }
+
+    @MessagePattern('createUserByEmail')
+    @UseFilters(new UserExceptionFilter())
+    @UseInterceptors(JsonInterceptor)
+    async createUserByEmail(@Payload(new CreateUserPipe()) userToCreate: CreateUserDto): Promise<IUserRes> {
+        return this.userService.createUserByEmail(userToCreate);
     }
 }
