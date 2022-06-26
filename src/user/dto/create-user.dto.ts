dc439d0f6f64f8eaeecab6f58c37aaaf5fc43943
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-06-26 22:15:42 +0700

    update dto with class-validator and @nestjs/mapped-types

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,23 @@
+import {
+    IsEmail,
+    IsString,
+    IsMobilePhone,
+    MinLength,
+    IsOptional,
+} from 'class-validator';
+
 export class CreateUserDto {
+    @IsString()
     public readonly login: string;
+
+    @IsEmail()
     public readonly email: string;
+
+    @IsString()
+    @MinLength(9)
     public readonly password: string;
+
+    @IsMobilePhone()
+    @IsOptional()
     public readonly phone?: string;
 }
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,8 +1,17 @@
-export class UpdateUserDto {
-    public readonly login?: string;
-    public readonly email?: string;
-    public readonly password?: string;
-    public readonly phone?: string;
+import { PartialType, OmitType } from '@nestjs/mapped-types';
+import { IsString, IsBoolean, IsOptional } from 'class-validator';
+import { CreateUserDto } from './create-user.dto';
+
+export class UpdateUserDto extends PartialType(
+    OmitType(CreateUserDto, ['login', 'email', 'password'] as const),
+) {
+    @IsString()
+    public readonly login: string;
+
+    @IsString()
+    @IsOptional()
     public readonly confirmationCode?: string | null;
+
+    @IsBoolean()
     public readonly confirmed?: boolean;
 }
